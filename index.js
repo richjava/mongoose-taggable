19582c1d0af2d7e0bcf6ab433489acc32cc919be
Richard Lovell <richjavalabs@gmail.com>
2017-02-11 20:42:01 +1300

    Added functionality to initiate plugin with multiple tag paths.

--- index.js
+++ index.js
@@ -13,238 +13,248 @@ var _ = require('underscore');
  *
  * @param {Schema} schema
  * @param {Object} [pluginOptions]
- * @param {String} [pluginOptions.path]           Path/key where tags will be stored. Default: 'tags'
-*/
-module.exports = function(schema, pluginOptions) {
-  pluginOptions = _.extend({
-    path: 'tags'
-  }, pluginOptions);
-
-  //Add paths to schema
-  var paths = {};
-  paths[pluginOptions.path] = [String];
-
-  schema.add(paths);
-
-
-  /**
-   * Add a tag
-   * 
-   * If a callback is passed, the change is made on the database (atomically).
-   *
-   * If no callback is passed, the change is made on the local (in memory) instance
-   * of the model. The model must be saved afterwards in order to persist to the DB.
-   *
-   * @param {String} tag      The tag to add
-   * @param {Function} [cb]   Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
-   *
-   * @return {Boolean}        If no callback is passed: False if tag already existed; true if added
-   */
-  schema.methods.addTag = function(path, tag, cb) {
-    if (arguments.length == 3) { //tag, cb
-      addTag_async(this, path, tag, cb);
-    } else { //tag
-      return addTag_sync(this, path, tag);
-    }
-  };
-
-
-  /**
-   * Remove a tag
-   * 
-   * If a callback is passed, the change is made on the database (atomically).
-   *
-   * If no callback is passed, the change is made on the local (in memory) instance
-   * of the model. The model must be saved afterwards in order to persist to the DB.
-   *
-   * @param {String} tag      The tag to remove
-   * @param {Function} [cb]   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
-   *
-   * @return {Boolean}        If no callback is passed: False if tag didn't exist; true if removed
-   */
-  schema.methods.removeTag = function(path, tag, cb) {
-    if (arguments.length == 3) { //tag, cb
-      removeTag_async(this, path, tag, cb);
-    } else { //tag
-      return removeTag_sync(this, path, tag);
-    }
-  };
-
-
-  /**
-   * Returns whether the document as a given tag
-   *
-   * @param {String} tag
-   *
-   * @return {Boolean}
-   */
-  schema.methods.hasTag = function(path, tag) {
-    var tags = this[path];
-
-    return _.contains(tags, tag);
-  };
-
-
-  /**
-   * Alters a query to filter by tags.
-   *
-   * @param {Query} query           Mongoose query object
-   * @param {String[]} includeTags  Tags the document must have
-   * @param {String[]} excludeTags  Tags the document must NOT have
-   *
-   * @return {Query}
-   */
-  schema.statics.filterByTags = function(query, includeTags, excludeTags) {
-    var path = pluginOptions.path;
-
-    var conditions = [];
-
-    //includeTags
-    if (includeTags && includeTags.length) {
-      var includeCondition = {};
-
-      includeCondition[path] = { $all: includeTags }
-
-      conditions.push(includeCondition);
-    }
-
-    //excludeTags
-    if (excludeTags && excludeTags.length) {
-      var excludeCondition = {};
-
-      excludeCondition[path] = {
-        $not: {
-          $in: excludeTags
+ * @param {Array} [pluginOptions.paths] Paths/keys where tags will be stored. Default: ['tags']
+ */
+module.exports = function (schema, pluginOptions) {
+    var paths = {};
+    if (pluginOptions.hasOwnProperty('paths') && Array.isArray(pluginOptions.paths)) {
+        for (var i = 0; i < pluginOptions.paths.length; i++) {
+            if (typeof pluginOptions.paths[i] === 'string') {
+                paths[pluginOptions.paths[i]] = [String];
+            } else {
+                paths['tags'] = [String];
+                break;
+            }
         }
-      }
-
-      conditions.push(excludeCondition);
-    }
-
-    //Add to query
-    if (conditions.length) {
-      query.and(conditions);
-    }
-
-    return query;
-  }
-
-  //=====================================================================
-
-  //PRIVATE FUNCTIONS
-
-  /**
-   * Add a tag to a document atomically on the database (async)
-   * 
-   * NOTE: This method modifies the document on the database
-   *
-   * @param {Model} self      The model instance
-   * @param {String} tag      The tag to add
-   * @param {Function} cb     Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
-   */
-  function addTag_async(self, path, tag, cb) {
-
-    //Find the doc  to update if it doesn't have the tag
-    var conditions = {};
-    conditions._id = self._id;
-    conditions[path] = { $ne: tag };
-
-    //Add the tag to the model
-    var update = {};
-    update.$push = {};
-    update.$push[path] = tag;
-
-    self.constructor.update(conditions, update, function(err, numDocsChanged) {
-      if (err) return cb(err);
-
-      var addedTag = numDocsChanged ? true : false;
-
-      //Add to the local (this) instance
-      if (addedTag) self[path].push(tag);
-
-      cb(null, addedTag);
-    });
-  }
-
-
-  /**
-   * Add a tag to a document in memory only (sync)
-   *
-   * @param {Model} self      The model instance
-   * @param {String} tag      The tag to add
-   *
-   * @return {Boolean}        False if tag already existed; true if added
-   */
-  function addTag_sync(self, path, tag) {
-
-    var alreadyExists = _.contains(self[path], tag);
-
-    if (alreadyExists) {
-      return false;
     } else {
-      //Add the tag
-      self[path].push(tag);
-
-      return true;
+        paths['tags'] = [String];
     }
-  }
+    //Add paths to schema
+    schema.add(paths);
 
 
-  /**
-   * Remove a tag from a document atomically on the database (async)
-   * 
-   * NOTE: This method modifies the document on the database
-   *
-   * @param {Model} self      The model instance
-   * @param {String} tag      The tag to add
-   * @param {Function} [cb]   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
-   */
-  function removeTag_async(self, path, tag, cb) {
-
-    //Find the doc  to update if it doesn't have the tag
-    var conditions = {};
-    conditions._id = self._id;
-    conditions[path] = tag;
-
-    //Remove the tag from the model
-    var update = {};
-    update.$pull = {};
-    update.$pull[path] = tag;
-
-    self.constructor.update(conditions, update, function(err, numDocsChanged) {
-      if (err) return cb(err);
-
-      var removedTag = numDocsChanged ? true : false;
-
-      //Remove from the local (this) instance
-      if (removedTag) self[path].splice(self[path].indexOf(tag), 1);
-
-      cb(null, removedTag);
-    });
-  }
+    /**
+     * Add a tag
+     * 
+     * If a callback is passed, the change is made on the database (atomically).
+     *
+     * If no callback is passed, the change is made on the local (in memory) instance
+     * of the model. The model must be saved afterwards in order to persist to the DB.
+     *
+     * @param {String} tag      The tag to add
+     * @param {Function} [cb]   Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
+     *
+     * @return {Boolean}        If no callback is passed: False if tag already existed; true if added
+     */
+    schema.methods.addTag = function (path, tag, cb) {
+        if (arguments.length == 3) { //tag, cb
+            addTag_async(this, path, tag, cb);
+        } else { //tag
+            return addTag_sync(this, path, tag);
+        }
+    };
 
 
-  /**
-   * Remove a tag from the model in memory only (sync)
-   *
-   * @param {Model} self      The model instance
-   * @param {String} tag      The tag to add
-   *
-   * @return {Boolean}        False if tag didn't exist; true if removed
-   */
-  function removeTag_sync(self, path, tag) {
+    /**
+     * Remove a tag
+     * 
+     * If a callback is passed, the change is made on the database (atomically).
+     *
+     * If no callback is passed, the change is made on the local (in memory) instance
+     * of the model. The model must be saved afterwards in order to persist to the DB.
+     *
+     * @param {String} tag      The tag to remove
+     * @param {Function} [cb]   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
+     *
+     * @return {Boolean}        If no callback is passed: False if tag didn't exist; true if removed
+     */
+    schema.methods.removeTag = function (path, tag, cb) {
+        if (arguments.length == 3) { //tag, cb
+            removeTag_async(this, path, tag, cb);
+        } else { //tag
+            return removeTag_sync(this, path, tag);
+        }
+    };
 
-    var alreadyExists = _.contains(self[path], tag);
 
-    if (!alreadyExists) {
-      return false;
-    } else {
-      //Remove the tag
-      var index = self[path].indexOf(tag);
+    /**
+     * Returns whether the document as a given tag
+     *
+     * @param {String} tag
+     *
+     * @return {Boolean}
+     */
+    schema.methods.hasTag = function (path, tag) {
+        var tags = this[path];
 
-      self[path].splice(index, 1);
+        return _.contains(tags, tag);
+    };
 
-      return true;
+
+    /**
+     * Alters a query to filter by tags.
+     *
+     * @param {Query} query           Mongoose query object
+     * @param {String[]} includeTags  Tags the document must have
+     * @param {String[]} excludeTags  Tags the document must NOT have
+     *
+     * @return {Query}
+     */
+    schema.statics.filterByTags = function (query, includeTags, excludeTags) {
+        var path = pluginOptions.path;
+
+        var conditions = [];
+
+        //includeTags
+        if (includeTags && includeTags.length) {
+            var includeCondition = {};
+
+            includeCondition[path] = {$all: includeTags}
+
+            conditions.push(includeCondition);
+        }
+
+        //excludeTags
+        if (excludeTags && excludeTags.length) {
+            var excludeCondition = {};
+
+            excludeCondition[path] = {
+                $not: {
+                    $in: excludeTags
+                }
+            }
+
+            conditions.push(excludeCondition);
+        }
+
+        //Add to query
+        if (conditions.length) {
+            query.and(conditions);
+        }
+
+        return query;
     }
-  }
+
+    //=====================================================================
+
+    //PRIVATE FUNCTIONS
+
+    /**
+     * Add a tag to a document atomically on the database (async)
+     * 
+     * NOTE: This method modifies the document on the database
+     *
+     * @param {Model} self      The model instance
+     * @param {String} tag      The tag to add
+     * @param {Function} cb     Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
+     */
+    function addTag_async(self, path, tag, cb) {
+
+        //Find the doc  to update if it doesn't have the tag
+        var conditions = {};
+        conditions._id = self._id;
+        conditions[path] = {$ne: tag};
+
+        //Add the tag to the model
+        var update = {};
+        update.$push = {};
+        update.$push[path] = tag;
+
+        self.constructor.update(conditions, update, function (err, numDocsChanged) {
+            if (err)
+                return cb(err);
+
+            var addedTag = numDocsChanged ? true : false;
+
+            //Add to the local (this) instance
+            if (addedTag)
+                self[path].push(tag);
+
+            cb(null, addedTag);
+        });
+    }
+
+
+    /**
+     * Add a tag to a document in memory only (sync)
+     *
+     * @param {Model} self      The model instance
+     * @param {String} tag      The tag to add
+     *
+     * @return {Boolean}        False if tag already existed; true if added
+     */
+    function addTag_sync(self, path, tag) {
+
+        var alreadyExists = _.contains(self[path], tag);
+
+        if (alreadyExists) {
+            return false;
+        } else {
+            //Add the tag
+            self[path].push(tag);
+
+            return true;
+        }
+    }
+
+
+    /**
+     * Remove a tag from a document atomically on the database (async)
+     * 
+     * NOTE: This method modifies the document on the database
+     *
+     * @param {Model} self      The model instance
+     * @param {String} tag      The tag to add
+     * @param {Function} [cb]   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
+     */
+    function removeTag_async(self, path, tag, cb) {
+
+        //Find the doc  to update if it doesn't have the tag
+        var conditions = {};
+        conditions._id = self._id;
+        conditions[path] = tag;
+
+        //Remove the tag from the model
+        var update = {};
+        update.$pull = {};
+        update.$pull[path] = tag;
+
+        self.constructor.update(conditions, update, function (err, numDocsChanged) {
+            if (err)
+                return cb(err);
+
+            var removedTag = numDocsChanged ? true : false;
+
+            //Remove from the local (this) instance
+            if (removedTag)
+                self[path].splice(self[path].indexOf(tag), 1);
+
+            cb(null, removedTag);
+        });
+    }
+
+
+    /**
+     * Remove a tag from the model in memory only (sync)
+     *
+     * @param {Model} self      The model instance
+     * @param {String} tag      The tag to add
+     *
+     * @return {Boolean}        False if tag didn't exist; true if removed
+     */
+    function removeTag_sync(self, path, tag) {
+
+        var alreadyExists = _.contains(self[path], tag);
+
+        if (!alreadyExists) {
+            return false;
+        } else {
+            //Remove the tag
+            var index = self[path].indexOf(tag);
+
+            self[path].splice(index, 1);
+
+            return true;
+        }
+    }
 };

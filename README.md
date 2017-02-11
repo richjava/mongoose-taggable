a988e37bd344850cc30bcf08dd35b113d5baf57d
Richard Lovell <richjavalabs@gmail.com>
2017-02-11 23:50:44 +1300

    Changed method signatures to accept optional path and updated readme.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 #mongoose-taggable
 
+Forked from [here](https://github.com/powmedia/mongoose-taggable) and the original 
+[here](https://github.com/aaronfrey/mongoose-taggable). Extended to allow support for 
+multiple tags.
+
 A simple tagging plugin for Mongoose
 
 Adds the ability to add and remove tags to a document, and find models filtered by tags.
@@ -7,6 +11,22 @@ Uses safe atomic updates to avoid race conditions.
 
 NOTE: Indexes are not applied to the tags path, because you should probably
 use a compound index with other paths in your schema
+
+
+
+##Set up
+In your model, require mongoose-taggable:
+```
+taggable = require('mongoose-taggable')
+```
+
+Then add the plugin to your model. You can add any number of tags using the "path" property of the pluginOptions object. In the example below, two tags will be added to the CourseSchema model, one called "tags" and the other called "prerequisites".
+```
+var CourseSchema = new Schema({
+});
+CourseSchema.plugin(taggable, { paths: ['tags','prerequisites'] });
+mongoose.model('Course', CourseSchema);
+```
 
 ##API
 
@@ -18,20 +38,21 @@ Add a tag (in memory only)
 - @return {Boolean}       False if tag already existed; true if added
 
 
-###model.addTag(tag, cb)
+###model.addTag(path, tag, cb)
 Add a tag to a document atomically
 
 NOTE: This method modifies the document on the database
 
 - @param {String} tag    The tag to add
-- @param {Function} cb   Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
+- @param {String} [path]   Optional. The property name of the model where the tags will be stored
+- @param {Function} [cb]   Optional. Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
 
 
 ###model.removeTag(tag)
 Remove a tag (in memory only)
 
 - @param {String} tag     The tag to add
-
+- @param {String} [path]  Optional. The property name of the model where the tags will be stored
 - @return {Boolean}       False if tag didn't exist; true if removed
 
 
@@ -41,14 +62,15 @@ Remove a tag from a document atomically
 NOTE: This method modifies the document on the database
 
 - @param {String} tag    The tag to remove
-- @param {Function} cb   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
+- @param {String} [path]   Optional. The property name of the model where the tags will be stored
+- @param {Function} [cb]   Optional. Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
 
 
 ###model.hasTag(tag)
 Returns whether the document as a given tag
 
 - @param {String} tag
-
+- @param {String} [path]   Optional. The property name of the model where the tags will be stored
 - @return {Boolean}
 
 
@@ -58,9 +80,11 @@ Alters a query to filter by tags.
 - @param {Query} query            Mongoose query object
 - @param {String[]} includeTags   Tags the document must have (must have all)
 - @param {String[]} excludeTags   Tags the document must NOT have (must not have any)
+- @param {String} [path]   Optional. The property name of the model where the tags will be stored
 
 
 
 ##Changelog
 ###v0.5.0
 - Changed the way excludeTags works in filterByTags(), from an AND operation to an OR operation. The method now selects items that have ALL of the includeTags but filters out items that have ANY of the excludeTags.
+- Added functionality to have multiple tags on a model.
--- index.js
+++ index.js
@@ -16,18 +16,19 @@ var _ = require('underscore');
  * @param {Array} [pluginOptions.paths] Paths/keys where tags will be stored. Default: ['tags']
  */
 module.exports = function (schema, pluginOptions) {
+    var DEFAULT_PATH = 'tags';
     var paths = {};
     if (pluginOptions.hasOwnProperty('paths') && Array.isArray(pluginOptions.paths)) {
         for (var i = 0; i < pluginOptions.paths.length; i++) {
             if (typeof pluginOptions.paths[i] === 'string') {
                 paths[pluginOptions.paths[i]] = [String];
             } else {
-                paths['tags'] = [String];
+                paths[DEFAULT_PATH] = [String];
                 break;
             }
         }
     } else {
-        paths['tags'] = [String];
+        paths[DEFAULT_PATH] = [String];
     }
     //Add paths to schema
     schema.add(paths);
@@ -41,16 +42,17 @@ module.exports = function (schema, pluginOptions) {
      * If no callback is passed, the change is made on the local (in memory) instance
      * of the model. The model must be saved afterwards in order to persist to the DB.
      *
-     * @param {String} tag      The tag to add
-     * @param {Function} [cb]   Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
-     *
+     * @param {String} tag      The tag value to add
+     * @param {String} [path]   Optional. The property name of the model where the tags will be stored
+     * @param {Function} [cb]   Optional. Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
      * @return {Boolean}        If no callback is passed: False if tag already existed; true if added
      */
-    schema.methods.addTag = function (path, tag, cb) {
-        if (arguments.length == 3) { //tag, cb
-            addTag_async(this, path, tag, cb);
+    schema.methods.addTag = function (tag, path, cb) {
+        path = path || DEFAULT_PATH;
+        if (cb) {
+            addTag_async(this, tag, path, cb);
         } else { //tag
-            return addTag_sync(this, path, tag);
+            return addTag_sync(this, tag, path);
         }
     };
 
@@ -64,13 +66,14 @@ module.exports = function (schema, pluginOptions) {
      * of the model. The model must be saved afterwards in order to persist to the DB.
      *
      * @param {String} tag      The tag to remove
-     * @param {Function} [cb]   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
-     *
+     * @param {String} [path]   Optional. The property name of the model where the tags will be stored
+     * @param {Function} [cb]   Optional. Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
      * @return {Boolean}        If no callback is passed: False if tag didn't exist; true if removed
      */
-    schema.methods.removeTag = function (path, tag, cb) {
-        if (arguments.length == 3) { //tag, cb
-            removeTag_async(this, path, tag, cb);
+    schema.methods.removeTag = function (tag, path, cb) {
+        path = path || DEFAULT_PATH;
+        if (cb) { //tag, cb
+            removeTag_async(this, tag, path, cb);
         } else { //tag
             return removeTag_sync(this, path, tag);
         }
@@ -78,15 +81,15 @@ module.exports = function (schema, pluginOptions) {
 
 
     /**
-     * Returns whether the document as a given tag
-     *
-     * @param {String} tag
-     *
+     * Returns whether the document as a given tag 
+     * @param {String} tag      The tag to find.
+     * @param {String} [path]   Optional. The property name of the model where the tags will be stored
      * @return {Boolean}
      */
-    schema.methods.hasTag = function (path, tag) {
+    schema.methods.hasTag = function (tag, path) {
+        path = path || DEFAULT_PATH;
         var tags = this[path];
-
+        
         return _.contains(tags, tag);
     };
 
@@ -97,11 +100,13 @@ module.exports = function (schema, pluginOptions) {
      * @param {Query} query           Mongoose query object
      * @param {String[]} includeTags  Tags the document must have
      * @param {String[]} excludeTags  Tags the document must NOT have
+     * @param {String} [path]         Optional. The property name of the model where the tags will be stored
      *
      * @return {Query}
      */
-    schema.statics.filterByTags = function (query, includeTags, excludeTags) {
-        var path = pluginOptions.path;
+    schema.statics.filterByTags = function (query, includeTags, excludeTags, path) {
+
+        path = path || DEFAULT_PATH;
 
         var conditions = [];
 
@@ -146,9 +151,10 @@ module.exports = function (schema, pluginOptions) {
      *
      * @param {Model} self      The model instance
      * @param {String} tag      The tag to add
-     * @param {Function} cb     Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
+     * @param {String} [path]   Optional. The property name of the model where the tags will be stored
+     * @param {Function} cb     Optional. Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
      */
-    function addTag_async(self, path, tag, cb) {
+    function addTag_async(self, tag, path, cb) {
 
         //Find the doc  to update if it doesn't have the tag
         var conditions = {};
@@ -180,10 +186,10 @@ module.exports = function (schema, pluginOptions) {
      *
      * @param {Model} self      The model instance
      * @param {String} tag      The tag to add
-     *
+     * @param {String} [path]   Optional. The property name of the model where the tags will be stored
      * @return {Boolean}        False if tag already existed; true if added
      */
-    function addTag_sync(self, path, tag) {
+    function addTag_sync(self, tag, path) {
 
         var alreadyExists = _.contains(self[path], tag);
 
@@ -205,9 +211,10 @@ module.exports = function (schema, pluginOptions) {
      *
      * @param {Model} self      The model instance
      * @param {String} tag      The tag to add
-     * @param {Function} [cb]   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
+     * @param {String} [path]   Optional. The property name of the model where the tags will be stored
+     * @param {Function} [cb]   Optional. Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
      */
-    function removeTag_async(self, path, tag, cb) {
+    function removeTag_async(self, tag, path, cb) {
 
         //Find the doc  to update if it doesn't have the tag
         var conditions = {};
@@ -239,10 +246,10 @@ module.exports = function (schema, pluginOptions) {
      *
      * @param {Model} self      The model instance
      * @param {String} tag      The tag to add
-     *
+     * @param {String} [path]   Optional. The property name of the model where the tags will be stored
      * @return {Boolean}        False if tag didn't exist; true if removed
      */
-    function removeTag_sync(self, path, tag) {
+    function removeTag_sync(self, tag, path) {
 
         var alreadyExists = _.contains(self[path], tag);
 
